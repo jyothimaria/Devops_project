1d36186e719598507253262d6b7af2d9b18cbb4b
jyothimaria <jyothimariastj@gmail.com>
2023-10-02 13:20:02 -0400

    adding project to the repository

---
+++ database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+       host : 'localhost',
+       database : 'devopsproject_1',
+       user : 'devops_proj_1',
+       password : 'Devops_project_1'
+
+
+});
+
+connection.connect(function(error){
+    if(error)
+    {
+        throw error;
+    }
+    else
+    {
+        console.log('MySQL Database is connected Successfully');
+    }
+
+
+
+
+});
+
+module.exports = connection;
